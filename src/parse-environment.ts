d584ec03b86f1b0d4ebd7d59360b4b40f1aba4ab
Felix A. Gladisch <felix@felixgladisch.de>
2021-08-26 15:21:43 +0200

    ⬆️ upgrade bridge lib

--- src/parse-environment.ts
+++ src/parse-environment.ts
@@ -1,31 +1,31 @@
 export interface OAuth2Options {
-	clientId: string;
-	clientSecret: string;
-	redirectUrl: string;
+  clientId: string;
+  clientSecret: string;
+  redirectUrl: string;
 }
 
 const {
-	PODIO_CLIENT_ID: clientId,
-	PODIO_CLIENT_SECRET: clientSecret,
-	PODIO_REDIRECT_URL: redirectUrl
+  PODIO_CLIENT_ID: clientId,
+  PODIO_CLIENT_SECRET: clientSecret,
+  PODIO_REDIRECT_URL: redirectUrl,
 } = process.env;
 
 export default function parseEnvironment(): OAuth2Options {
-	if (!clientId) {
-		throw new Error("Missing client ID in environment.");
-	}
+  if (!clientId) {
+    throw new Error("Missing client ID in environment.");
+  }
 
-	if (!clientSecret) {
-		throw new Error("Missing client secret in environment.");
-	}
+  if (!clientSecret) {
+    throw new Error("Missing client secret in environment.");
+  }
 
-	if (!redirectUrl) {
-		throw new Error("Missing redirect URI in environment.");
-	}
+  if (!redirectUrl) {
+    throw new Error("Missing redirect URI in environment.");
+  }
 
-	return {
-		clientId,
-		clientSecret,
-		redirectUrl
-	};
+  return {
+    clientId,
+    clientSecret,
+    redirectUrl,
+  };
 }
